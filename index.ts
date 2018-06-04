3a82e7a6f773bf00cf1b98d8b4f43f45b9225e59
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-06-04 11:40:26 +0200

    Updating exports

--- index.ts
+++ index.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { OTREE_MODULES } from './src/components';
-// import { OTREE_PROVIDERS } from './src/services';
 
 /**
  * Exports
  */
 export * from './src/components';
-// export * from './src/core';
-// export * from './src/interfaces';
-// export * from './src/services';
 
 @NgModule({
   imports: OTREE_MODULES,
   exports: OTREE_MODULES
-  // ,
-  // providers: OTREE_PROVIDERS
 })
 export class OChartModule {
 }
--- src/components.ts
+++ src/components.ts
@@ -1,9 +1,10 @@
 import { OTreeModule } from './components/tree/o-tree.component';
 import { OTreeNodeModule } from './components/tree/o-tree-node.component';
 
-export * from './components/tree/tree.components';
+export * from './components/tree/o-tree.component';
+export * from './components/tree/o-tree-node.component';
 
 export const OTREE_MODULES: any[] = [
-  OTreeComponentModule,
+  OTreeModule,
   OTreeNodeModule
 ];
